17f7568bfea9a6d2ac211ebbdaade0cb402ff28a
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-09-13 13:07:26 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/Persona/Persona.java
@@ -0,0 +1,21 @@
+package com.example.demo.Persona;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Persona {
+
+    String id;
+    String nome;
+    String cognome;
+    String email;
+
+
+
+}
